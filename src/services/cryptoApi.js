f4a47d18b11cc5facb58136b281643a5e2fa43e5
Callum Macalast <calmacalast@gmail.com>
2021-12-02 22:12:02 +0000

    Created services to make requests

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,16 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'cf225ebf12msh138864c42a40a7ep1b9308jsn378d75308348'
+}
 
-// var options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/stats',
-//   headers: {
-//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-//     'x-rapidapi-key': 'cf225ebf12msh138864c42a40a7ep1b9308jsn378d75308348'
-//   }
-// };
+const baseUrl = 'https://coinranking1.p.rapidapi.com/'
 
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})
