61660083b3de8480731d8679def644179a4b7d64
Callum Macalast <calmacalast@gmail.com>
2021-12-02 20:59:06 +0000

    Create layout and other sections, create redux store state

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,16 @@
+
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/stats',
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'cf225ebf12msh138864c42a40a7ep1b9308jsn378d75308348'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
